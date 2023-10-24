87a245aec8b7ac2a2479e693a191863cab050138
vveskr <vveskr@gmail.com>
2023-10-24 09:40:03 +0700

    update

--- ArrayNilai17.java
+++ ArrayNilai17.java
@@ -5,13 +5,16 @@ public class ArrayNilai17 {
         int[] nilaiAkhir = new int[10];
 
         //perulangan
-        for(int i = 0; i<10; i++){
-            System.out.print("Masukkan nilai akhir ke-"+i+ " : ");
-            nilaiAkhir[i] = sc.nextInt();
+        for(int i = 0; i <nilaiAkhir.length; i++){
+            System.out.print("Masukkan nilai akhir ke-" +i+ " : ");
+            nilaiAkhir[i] = sc.nextInt();}
+            for(int i=0; i < nilaiAkhir.length; i++){
+            if(nilaiAkhir[i]>70){
+                System.out.println("Mahasiswa ke-"+i+" lulus! ");
+            }else{
+                System.out.println("Mahasiswa ke-"+i+" tidak lulus!");
+            }
         }
-        for(int i = 0; i<10; i++){
-            System.out.println("nilai akhir ke-" +i+ " adalah "+nilaiAkhir[i]);
         }
+        
     }
-    
-}
