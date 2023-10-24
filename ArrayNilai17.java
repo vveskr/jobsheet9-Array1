52a5dfaf77e39b07dbec44b33db6ae1c392f887c
vveskr <vveskr@gmail.com>
2023-10-24 10:42:20 +0700

    update

--- ArrayNilai17.java
+++ ArrayNilai17.java
@@ -7,7 +7,8 @@ public class ArrayNilai17 {
         //perulangan
         for(int i = 0; i <nilaiAkhir.length; i++){
             System.out.print("Masukkan nilai akhir ke-" +i+ " : ");
-            nilaiAkhir[i] = sc.nextInt();}
+            nilaiAkhir[i] = sc.nextInt();
+        }
             for(int i=0; i < nilaiAkhir.length; i++){
             if(nilaiAkhir[i]>70){
                 System.out.println("Mahasiswa ke-"+i+" lulus! ");
--- ArrayRataNilai17.java
+++ ArrayRataNilai17.java
@@ -2,20 +2,33 @@ import java.util.Scanner;
 public class ArrayRataNilai17 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int[] nilaiMhs = new int[10];
-        double total = 0;
-        double rata2;
-
+        int jmlMhs;
+        int jmlLulus=0, jmlTidakLulus=0;
+        double rata2lulus=0, rata2TidakLulus=0;
+        
         //array
+        System.out.print("Masukkan jumlah mahasiswa : ");
+        jmlMhs = sc.nextInt();
+        int[] nilaiMhs = new int[jmlMhs];
         for(int i = 0; i<nilaiMhs.length; i++){
             System.out.print("Masukkan nilai mahasiswa ke-" +(i+1)+" : ");
             nilaiMhs[i] = sc.nextInt();
-            
-        }
-        for(int i = 0; i<nilaiMhs.length; i++){
-                total += nilaiMhs[i];
+         }
+        
+            for(int i = 0; i<jmlMhs; i++){
+                if(nilaiMhs[i]>=70){
+                    rata2lulus+=nilaiMhs[i];
+                    jmlLulus++;
+                }else{
+                    rata2TidakLulus+=nilaiMhs[i];
+                    jmlTidakLulus++;
+                }
             }
-            rata2 = total/nilaiMhs.length;
-            System.out.println("Rata-rata nilai = "+rata2);
+            rata2lulus /= jmlLulus;
+            rata2TidakLulus /= jmlTidakLulus;
+            System.out.println("Rata-rata nilai lulus: "+rata2lulus);
+            System.out.println("Rata-rata nilai tidak lulus: "+rata2TidakLulus);
     }
+       
+        
 }
